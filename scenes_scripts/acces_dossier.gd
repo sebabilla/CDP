ac4de80be0051781ce81d2fab4c9ce37c492563c
sebabilla <seb.abilla@gmail.com>
2025-01-05 00:17:49 +0100

    changement du nom du ddossier utilisateur

--- scenes_scripts/acces_dossier.gd
+++ scenes_scripts/acces_dossier.gd
@@ -2,7 +2,7 @@ extends Button
 
 func _on_pressed() -> void:
 	if OS.get_name() == "Linux":
-		OS.shell_open(OS.get_data_dir() + "/Plan de classe")
+		OS.shell_open(OS.get_data_dir() + "/Clan de place")
 	elif OS.get_name() == "Windows":
-		OS.shell_open(OS.get_data_dir() + "\\Plan de classe")
+		OS.shell_open(OS.get_data_dir() + "\\Clan de place")
 	
