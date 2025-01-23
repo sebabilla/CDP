c216e66e2a3dca5380c53cc933d9ee71eea84053
sebabilla <seb.abilla@gmail.com>
2025-01-23 23:29:31 +0100

    correction de bugs d'affichage lors de la rotation

---
+++ scripts/reactions.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func echec() -> void:
+	var racine: Node = get_tree().root.get_node("Main")
+	var tween: Tween = create_tween()
+	tween.tween_property(racine, "position", Vector2(5, 5), 0.05).from(Vector2.ZERO)
+	tween.tween_property(racine, "position", Vector2(-5, -5), 0.1).from(Vector2(5,5))
+	tween.tween_property(racine, "position", Vector2.ZERO, 0.05).from(Vector2(-5,-5))
